27480c219d190e95524aee88c7a51ef84218f290
Vic <vicmisanu63@gmail.com>
2025-03-11 01:53:02 +0100

    feat message of success added

--- src/components/Section6/Section6.tsx
+++ src/components/Section6/Section6.tsx
@@ -2,47 +2,49 @@ import { Mail } from 'lucide-react'
 import emailjs from "@emailjs/browser"
 import { useState } from 'react'
 
-export const Section6: React.FC<{}> = () => {
+export const Section6 = () => {
 
     const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID
     const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID
     const publicKey = import.meta.env.VITE_PUBLIC_KEY
 
-    const  [userName, setUserName] = useState('');
-  const [emailAddress, setEmailAddress] = useState('');
-  const [message, setMessage] = useState('');
+    const [userName, setUserName] = useState('');
+    const [emailAddress, setEmailAddress] = useState('');
+    const [message, setMessage] = useState('');
+    const [showPopup, setShowPopup] = useState(false);
 
-  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUserName(e.target.value);
-  };
+    const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserName(e.target.value);
+    };
 
-  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmailAddress(e.target.value);
-  };
+    const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmailAddress(e.target.value);
+    };
 
-  const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMessage(e.target.value);
-  };
+    const handleMessageChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.target.value);
+    };
+
     const handleSendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
+        e.preventDefault();
         emailjs.sendForm(serviceId, templateId, e.target as HTMLFormElement, publicKey)
+            .then(
+                (result) => {
+                    console.log('Message envoyé avec succès', result.text);
+                    setShowPopup(true);
+                },
+                (error) => {
+                    console.error('Erreur lors de l\'envoi du message', error.text);
+                }
+            );
 
-        .then(
-            (result) => {
-            console.log('Message envoyé avec succès', result.text);
-            },
-            (error) => {
-            console.error('Erreur lors de l\'envoi du message', error.text);
-            }
-        );
-        
         setUserName('');
         setEmailAddress('');
         setMessage('');
+    };
 
-    }
     return (
-        <div id="writeUs" className=" w-full h-auto flex flex-col px-6 pt-12">
+        <div id="writeUs" className="w-full h-auto flex flex-col px-6 pt-12">
             <div className="flex items-center gap-3 sm:gap-5 mb-8 sm:mb-12">
                 <div className="bg-gray-900 p-1.5 rounded-full">
                     <Mail className="w-8 sm:w-12 h-8 sm:h-12 text-white" />
@@ -61,14 +63,20 @@ export const Section6: React.FC<{}> = () => {
                             <input className='bg-black/10 w-full h-12 p-5 outline-none rounded-md text-black font-normal text-md lg:md:text-xl placeholder:text-slate-900' placeholder='Your email address' type="email" name='emailAddress' value={emailAddress} onChange={handleEmailChange}/>
                         </div>
                         <div id='message'>
-                            <textarea className='bg-black/10 w-full h-44 p-5 outline-none rounded-md text-black font-normal text-md lg:md:text-xl placeholder:text-slate-900' placeholder='Your message' name='message'value={message} onChange={handleMessageChange}/>
+                            <textarea className='bg-black/10 w-full h-44 p-5 outline-none rounded-md text-black font-normal text-md lg:md:text-xl placeholder:text-slate-900' placeholder='Your message' name='message' value={message} onChange={handleMessageChange}/>
                         </div>
                         <input className='bg-blue-950 w-full h-12 text-white font-bold hover:cursor-pointer hover:text-blue-950 hover:border-blue-950 border-2 hover:bg-transparent hover:shadow-lg rounded-xl shadow-black' type="submit" value="Send" />
-                                            
                     </div>
                 </form>
-                                
             </div>
+            {showPopup && (
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="bg-white p-6 rounded-md shadow-md">
+                        <h2 className="text-xl font-bold mb-4">Message sent successfully</h2>
+                        <button className="bg-blue-950 text-white px-4 py-2 rounded-md" onClick={() => setShowPopup(false)}>Fermer</button>
+                    </div>
+                </div>
+            )}
         </div>
-    )
-}
+    );
+};
